c215940bdd0d826e017ac7339466ba4d324113f0
amitay keisar <amiteyk3@gmail.com>
2025-06-15 15:46:01 +0300

    cleaned code

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,8 +3,8 @@ import { useLogin } from "./useLogin";
 
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
-import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import Input from "../../ui/Input";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
