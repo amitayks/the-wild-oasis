76d6f1bca7e3ad5c0f3473b224accb96e7bc1860
amitay keisar <amiteyk3@gmail.com>
2025-06-16 14:02:44 +0300

    refactor ui folder

--- src/ui/ConfirmDelete/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete/ConfirmDelete.jsx
@@ -1,24 +1,6 @@
-import styled from "styled-components";
-import Button from "./Button";
-import Heading from "./Heading";
-
-const StyledConfirmDelete = styled.div`
-  width: 40rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1.2rem;
-
-  & p {
-    color: var(--color-grey-500);
-    margin-bottom: 1.2rem;
-  }
-
-  & div {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
+import Button from "../Button";
+import Heading from "../Heading";
+import { StyledConfirmDelete } from "./styled";
 
 function ConfirmDelete({ resourceName, onConfirm, disabled, onCloseModal }) {
   return (
